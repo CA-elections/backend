fbfce727716496b6c80ace7920baa9b90206596b
Jan Kaifer <kaifer741@gmail.com>
2018-11-26 16:23:37 +0100

    corrected api endpoint

--- backend/urls.py
+++ backend/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+from django.views import generic
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^api', include('api.urls')),
+    url(r'^api$', generic.RedirectView.as_view(url='api/', permanent=True)),
+    url(r'^api/', include('api.urls')),
 ]
