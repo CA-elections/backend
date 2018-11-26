ba2868f3b73a66787aaf2f41c1b18679a07681f0
Jan Kaifer <kaifer741@gmail.com>
2018-11-26 15:55:23 +0100

    added all views

--- api/views.py
+++ api/views.py
@@ -14,3 +14,50 @@ class CandidateDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Candidate.objects.all()
     serializer_class = CandidateSerializer
 
+
+class ElectionList(generics.ListCreateAPIView):
+
+    queryset = Election.objects.all()
+    serializer_class = ElectionSerializers
+
+
+class ElectionDetails(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = Election.objects.all()
+    serializer_class = ElectionSerializers
+
+
+class NotificationList(generics.ListCreateAPIView):
+
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializers
+
+
+class NotificationDetails(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializers
+
+
+class VoteList(generics.ListCreateAPIView):
+
+    queryset = Vote.objects.all()
+    serializer_class = VoteSerializer
+
+
+class VoteDetails(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = Vote.objects.all()
+    serializer_class = VoteSerializer
+
+
+class ScoreList(generics.ListCreateAPIView):
+
+    queryset = Score.objects.all()
+    serializer_class = ScoreSerializer
+
+
+class ScoreDetails(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = Score.objects.all()
+    serializer_class = ScoreSerializer
