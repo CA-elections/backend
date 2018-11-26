b602d5bc51b9ed691873dd79e0b97dbc9568ba0f
Jan Kaifer <kaifer741@gmail.com>
2018-11-26 15:48:46 +0100

    created views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, views, response, exceptions, status, pagination
+from .serializers import CandidateSerializer, ElectionSerializers, NotificationSerializers, VoteSerializer, ScoreSerializer
+from .models import Candidate, Election, Notification, Vote, Score
 
-# Create your views here.
+
+class CandidateList(generics.ListCreateAPIView):
+
+    queryset = Candidate.objects.all()
+    serializer_class = CandidateSerializer
+
+
+class CandidateDetails(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = Candidate.objects.all()
+    serializer_class = CandidateSerializer
+
