b22c118f243ac1dac9bdf2d2ae793802e54d6063
Jan Kaifer <kaifer741@gmail.com>
2018-11-26 16:17:48 +0100

    installed pyyaml

--- backend/urls.py
+++ backend/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^api/', include('api.urls')),
+    url(r'^api', include('api.urls')),
 ]
