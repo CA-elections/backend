10de2cf7c7702d7e1f586029092cfb4847086265
CaField <CaField@LT91YLQC2a.ca.com>
2018-11-26 15:58:19 +0100

    Added SimpleEmailTestCase

--- mail/tests.py
+++ mail/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.core.mail import send_mail
 
 # Create your tests here.
+
+
+class SimpleEmailTestCase(TestCase):
+    def test_send_message(self):
+        send_mail("Test", "Email sending test", "volbytest@seznamq.cz", ["xsicp01@gjk.cz", "volbytest@seznam.cz"])
