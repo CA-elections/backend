e58d6b9d57d053781991f51e4923a3d26edc092c
CaField <CaField@LT91YLQC2a.ca.com>
2018-11-27 13:16:27 +0100

    Bugfixes

--- mail/tests.py
+++ mail/tests.py
@@ -13,7 +13,7 @@ class EmailTestCase(TestCase):
         Candidate.objects.create(name="John", surname="Smith", annotation="Example", is_student=True)
         self.e1 = Election.objects.create(name="Student election 1", date_start=datetime.datetime.now() - datetime.timedelta(1),
                                      date_end=datetime.datetime.now() + datetime.timedelta(1), is_student=True)
-        n1 = Notification.objects.create(code="ahoj", election=e1)
+        n1 = Notification.objects.create(code="ahoj", election=self.e1)
         Vote.objects.create(notification=n1, id_student=1)
 
     def test_send_student_election(self):
