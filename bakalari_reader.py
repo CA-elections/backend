b408c78eaa15b92188b9bc9fa7acb3c4f56f0519
CaField <CaField@LTD9VLHC2.ca.com>
2018-11-27 12:06:13 +0100

    Edits

--- bakalari_reader.py
+++ bakalari_reader.py
@@ -6,6 +6,8 @@ conn = sqlite3.connect('bakalari.db')
 def get_parent_email(student_id):
     return conn.cursor().execute("SELECT \"Parent Email\" FROM bakalari WHERE ID=(?);", [str(student_id)]).fetchone()[0]
 
+def get_student_email(student_id):
+    return conn.cursor().execute("SELECT Email FROM bakalari WHERE ID=(?);", [str(student_id)]).fetchone()[0]
 
 def get_birthdate(student_id):
     day, month, year = map(int, conn.cursor().execute("SELECT Birthdate FROM bakalari WHERE ID=(?);",
@@ -13,9 +15,14 @@ def get_birthdate(student_id):
     return datetime.date(year, month, day)
 
 
-def get_students_with_parent_email(parent_email):
-    yield from map(int, map(lambda x: x[0], conn.cursor().execute("SELECT ID FROM bakalari WHERE \"Parent Email\"=(?);",
-                                                                  [parent_email])))
+def get_infants_with_parent_email(parent_email):
+    now = datetime.datetime.now()
+    dt = datetime.date(now.year - 18, now.month, now.day)
+    for i, birth in map(lambda x: x[0], conn.cursor().execute("SELECT ID, Birthdate FROM bakalari WHERE \"Parent Email\"=(?);", [parent_email])):
+        year, month, day = map(int, birth.split())
+        birthdate = datetime.date(year, month, day)
+        if dt <= birthdate:
+            yield i
 
 
 def get_students():
