a3d9d62d6b4a83b4d3f2cad618483419e6bba96a
Jan Kaifer <kaifer741@gmail.com>
2018-11-27 09:35:47 +0100

    forked notification serializers and corrected TYPOs

--- api/models.py
+++ api/models.py
@@ -16,7 +16,7 @@ class Election(models.Model):
 
 
 class Notification(models.Model):
-    election = models.ForeignKey(Election, on_delete=models.CASCADE, related_name='notifiactions')
+    election = models.ForeignKey(Election, on_delete=models.CASCADE, related_name='notifications')
 
     sent = models.BooleanField(default=False)
     code = models.CharField(max_length=100)
