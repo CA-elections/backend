b1584f63fe5a619e909a6700d38be61dc6154ab5
CaField <CaField@LT91YLQC2a.ca.com>
2018-11-27 11:13:11 +0100

    Bugfixes

--- bakalari_reader.py
+++ bakalari_reader.py
@@ -1,13 +1,18 @@
 import sqlite3
+import datetime
 conn = sqlite3.connect('bakalari.db')
 
 
 def get_parent_email(student_id):
-    return conn.cursor().execute("SELECT Parent Email FROM bakalari WHERE ID=(?);", str(student_id)).fetchone()[0]
+    return conn.cursor().execute("SELECT \"Parent Email\" FROM bakalari WHERE ID=(?);", str(student_id)).fetchone()[0]
 
 
 def get_birthdate(student_id):
-    return conn.cursor().execute("SELECT Birthdate FROM bakalari WHERE ID=(?);", str(student_id)).fetchone()[0]
+    return datetime.date(*(conn.cursor().execute("SELECT Birthdate FROM bakalari WHERE ID=(?);", str(student_id)).fetchone()[0].split(".")))
+
+
+def get_students_with_parent_email(parent_email):
+    yield from map(int, map(lambda x: x[0], conn.cursor().execute("SELECT ID FROM bakalari WHERE \"Parent Email\"=(?);", parent_email)))
 
 
 def get_students():
