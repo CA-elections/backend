71e9c310c88212c58301defaa40757679adc9424
CaField <CaField@gisloancode5.ca.com>
2018-11-27 15:05:22 +0100

    third test completed

--- api/tests.py
+++ api/tests.py
@@ -33,8 +33,9 @@ class CandidateTests(APITestCase):
     def test_read_candidate(self):
 
         for instance_data in self.mock_up_data:
-            instance = Candidate(instance_data)
+            instance = Candidate(id=1, **instance_data)
             response = self.client.get(self.url + str(instance.id) + "/", {}, format="json")
+            print(response.body)
             # Check status code
             self.assertEqual(response.status_code, status.HTTP_200_OK)
             # Check the response data
@@ -43,3 +44,21 @@ class CandidateTests(APITestCase):
             self.assertEqual(response_data["name"], instance.name)
             self.assertEqual(response_data["surname"], instance.surname)
             self.assertEqual(response_data["is_student"], instance.is_student)
+
+    def test_update_candidate(self):
+
+        for instance_data in self.mock_up_data:
+            for request_data in self.mock_up_data:
+                instance = Candidate(id=1, **instance_data)
+                response = self.client.put(self.url + str(instance.id) + "/", request_data, format="json")
+                # Check status code
+                self.assertEqual(response.status_code, status.HTTP_200_OK)
+                # Check the response data
+                response_data = loads(response.content)
+                for key in request_data:
+                    self.assertEqual(request_data[key], response_data[key])
+                # Check database data
+                self.assertEqual(request_data["annotation"], instance.annotation)
+                self.assertEqual(request_data["name"], instance.name)
+                self.assertEqual(request_data["surname"], instance.surname)
+                self.assertEqual(request_data["is_student"], instance.is_student)
