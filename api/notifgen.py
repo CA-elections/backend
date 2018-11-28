7d7b2241e5bcf63fd59b7fb6b9eed07323d6e537
mimo31 <viktor.fukala@email.cz>
2018-11-28 09:28:42 +0100

    Fixes

--- api/notifgen.py
+++ api/notifgen.py
@@ -5,7 +5,7 @@ import bakalari_reader
 
 def generate_notifications():
     now = datetime.datetime.now()
-    for an_election in Election.objects.filter(are_notifs_generated=False, date_start_gte=now):
+    for election in Election.objects.filter(are_notifs_generated=False, date_start_gte=now):
          if (not election.is_student):
             notifs = bakalari_reader.get_all_youth_by_parent()
             for notif in notifs:
@@ -21,4 +21,7 @@ def generate_notifications():
                 new_notification.save()
                 new_vote = Vote(notification=new_notification, id_student=idstud)
                 new_vote.save()
+        election.are_notifs_generated=True
+        election.save()
+        
        
