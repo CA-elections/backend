ef55bb1b623535684406e6d6bb28d5af1bbbf78c
Lukáš Veškrna <lukas.veskrna@gmail.com>
2018-11-28 11:18:54 +0100

    Added docstring for ElectionGetAllSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -7,6 +7,7 @@ import pytz
 import datetime
 import pytz
 
+
 class CandidateWriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Candidate
@@ -201,7 +202,16 @@ class ScoreSerializer(serializers.ModelSerializer):
 
 
 class ElectionGetAllSerializer(serializers.BaseSerializer):
-
+    """Build a list of elections in the format:
+       {
+         "id": id,
+         "date_start": date_start,
+         "date_end": date_end,
+         "is_student": is_student,
+         "description": description,
+         "name": name
+       }
+    """
     def to_representation(self, instance):
         data = {
             'id': instance.id,
