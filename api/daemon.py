eafb2d4d4d7f2932c6cbdb8c7e374639806fa60a
mimo31 <viktor.fukala@email.cz>
2018-11-29 09:17:27 +0100

    One-line change in daemon

--- api/daemon.py
+++ api/daemon.py
@@ -10,7 +10,7 @@ tz = timezone(settings.TIME_ZONE)
 
 def generate_notifications():
     now = datetime.datetime.now(tz)
-    for election in Election.objects.filter(are_notifs_generated=False, date_start__lte=now):
+    for election in Election.objects.filter(are_notifs_generated=False, date_start__lte=now, date_end__gt==now):
         log("generating notifications (and votes) for an election (name: " + election.name + ")")
         log("election started at " + str(election.date_start))
         if not election.is_student:
