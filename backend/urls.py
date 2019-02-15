fc67a02ecd797446f1fdea74cbb6c9d2eb7833ee
Lukáš Hozda <luk.hozda@gmail.com>
2019-02-15 10:41:22 +0100

    minor

--- backend/urls.py
+++ backend/urls.py
@@ -20,10 +20,11 @@ from django.views import generic
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+import os
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     url(r'^api$', generic.RedirectView.as_view(url='api/', permanent=True)),
     url(r'^api/', include('api.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static('/dist/', document_root=os.path.join(settings.BASE_DIR, 'dist'))
