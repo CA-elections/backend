92fba1ba993d0638d61737a4bf1e25f7e0511c53
Lukáš Hozda <luk.hozda@gmail.com>
2019-03-08 14:43:10 +0100

    register models with django admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Candidate, Election, Notification, Vote, Score
 # Register your models here.
+
+admin.site.register(Candidate)
+admin.site.register(Election)
+admin.site.register(Notification)
+admin.site.register(Vote)
+admin.site.register(Score)
